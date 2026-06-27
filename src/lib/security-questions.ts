efd9b800587d26c80cbbd454e708a0ff457de0d5
vercel <peteroalal@gmail.com>
2026-06-27 16:19:13 +0300

    Create security-questions.ts

---
+++ src/lib/security-questions.ts
@@ -0,0 +1,8 @@
+export const SECURITY_QUESTION_PRESETS = [
+  'What city were you born in?',
+  'What is your high school name?',
+  'What is your favourite colour?',
+  'Who is your best childhood friend?',
+] as const
+
+export const CUSTOM_QUESTION_VALUE = '__custom__'
